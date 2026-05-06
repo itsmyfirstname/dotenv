280b8dd8e7d313d02de06861bac19d3756739cac
itsmyfirstname <itsmyfirstname@pm.me>
2026-05-05 22:10:45 -0400

    more opencode configs

--- common/zsh/conf.zsh
+++ common/zsh/conf.zsh
@@ -5,7 +5,7 @@
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 #ZSH_THEME="funky"
-ZSH_THEME="agnoster"
+#ZSH_THEME="random"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
